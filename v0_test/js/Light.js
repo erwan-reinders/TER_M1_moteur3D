e7ef3d68650356f40a222a7c5369f96881566c62
Ange <ange.clement@etu.umontpellier.fr>
2022-04-12 10:00:34 +0200

    pull

--- v0_test/js/Light.js
+++ v0_test/js/Light.js
@@ -5,7 +5,7 @@ class Light {
      * @param pos : vec3 position de la lumière
      * @param color : vec3 couleur de la lumière (RGB) **/
     constructor(pos, color) {
-        this.position = pos ?? vec3.clone([1, 2, -1]);
+        this.position = pos ?? vec3.clone([2, 3, 1]);
         this.color = color ?? vec3.clone([1, 1, 1]);
     }
 }
--- v0_test/js/util.js
+++ v0_test/js/util.js
@@ -18,6 +18,18 @@ function initGl(canvasId) {
     }
 }
 
+// https://stackoverflow.com/questions/36921947/read-a-server-side-file-using-javascript
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+        result = xmlhttp.responseText;
+    }
+    return result;
+}
+
 //
 // Get the content from a HTML tag
 //
