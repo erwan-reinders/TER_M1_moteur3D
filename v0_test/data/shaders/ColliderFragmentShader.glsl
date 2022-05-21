3595d9073bee0741db1bfee9ec6ecffd242a4ea1
BuildTools <ange.clement@etu.umontpellier.fr>
2022-05-21 18:18:40 +0200

    marche po :(

--- v0_test/data/shaders/ColliderFragmentShader.glsl
+++ v0_test/data/shaders/ColliderFragmentShader.glsl
@@ -6,6 +6,6 @@ out vec4 FragColor;
 uniform vec3 uColor;
 
 void main(){
-    //FragColor = vec4(uColor,1);
-    FragColor = vec4(1,0,0,0);
+    FragColor = vec4(uColor,1);
+    //FragColor = vec4(1,0,0,1);
 }
